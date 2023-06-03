034739cafe3efd9fc842e6a67f747764084204a0
VeranikaBabik <veranikababik@gmail.com>
2023-06-03 15:07:44 +0200

    load more

--- src/scripts/pixabay.js
+++ src/scripts/pixabay.js
@@ -2,6 +2,7 @@ import { API_URL, DEFAULT_PIXABAY_PARAMS } from './configuration';
 import { btnLoadMore } from './main';
 import axios from 'axios';
 import Notiflix, { Notify } from 'notiflix';
+import { btnLoadMore } from './main';
 export default async function dataFromPixabay({ q = '', page = '1' }) {
   try {
     const query = {
@@ -13,20 +14,22 @@ export default async function dataFromPixabay({ q = '', page = '1' }) {
     const response = await axios.get(API_URL, { params: query });
     const { hits: photos, total } = response.data;
 
-    if (photos === 0 && q !== '') {
-      Notiflix.Notify.warning(
+    if (photos.length === 0 && page === '1') {
+      Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+      btnLoadMore.style.display = 'none';
       return;
     }
-    if (page > total / DEFAULT_PIXABAY_PARAMS.per_page) {
-      btnLoadMore.disabled = true;
+    if (page > Math.ceil(total / DEFAULT_PIXABAY_PARAMS.per_page)) {
+      btnLoadMore.style.display = 'none';
       Notiflix.Notify.warning(
         "We're sorry, but you've reached the end of search results."
       );
       return;
     }
     if (page === '1' && q !== '') {
+      btnLoadMore.style.display = 'flex';
       Notiflix.Notify.success(`Hooray! We found ${total} images.`);
     }
     return photos;
